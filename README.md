23c656761930be016e441eab517c88ca266cb730
MtendeKuyokwa <mtendekuyokwa19@gmail.com>
2023-12-28 21:02:45 +0200

    creation of layout

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Etch-A-Sketch
+# mobile menus
 
 Tools used
 
-* HTML5
-* CSS3
-* Javascript
-* Figma
+- HTML5
+- CSS3
+- Javascript
+- Figma
 
-*future updates may include addition of a message box*
+_future updates may include addition of a message box_
--- script.js
+++ script.js
@@ -1,111 +1,40 @@
-let parent=document.querySelector('#container')
-let grid=document.querySelector('.grid')
-let noGrid=document.querySelector('.noGrid')
-let black=document.querySelector('.black')
-let red=document.querySelector('.red')
-let green=document.querySelector('.green')
-let boxNum=document.querySelector('.boxSize')
-let size;
-let measurement;
-   do{
+// eslint-disable-next-line no-unused-vars
+const printOutName = (() => {
+  const navbtn = document.querySelectorAll('.iconbtn')
 
-           
-         measurement=function(){
-            size=prompt("enter box sizes between 17-99")
-            
-        }
-       measurement();
-       
+  const navTitle = document.querySelectorAll('.navTitle')
+  const iconsSvg = document.querySelectorAll('svg')
 
-   }while(!(size>16&&size<100))   
-let chosenColor=function (x){
-  
-    switch(x){
-        case 1:
-            color="black"
-            console.log ("black")
-            break;
-        case 2:
-            color="red"
-            console.log ("red")
-            break;
-        case 3:
-            color="green"
-            console.log ("green")
-            break;       
-        default:
-            color="blue"
-
+  function settingFunctiontobtns () {
+    for (let i = 0; i < navbtn.length; i++) {
+      navbtn[i].addEventListener('click', function () {
+        switchingDisplayOfbtns(i)
+      })
     }
-}
+  }
+  settingFunctiontobtns()
+  function showTitle (index) {
+    navTitle[index].style.setProperty('display', 'flex')
+  }
 
-chosenColor(2)
-black.addEventListener('click',function(e){
-
-    chosenColor(1);
-
-})
-red.addEventListener('click',function(e){
-
-
-    chosenColor(2)
-})
-
-green.addEventListener('click',function(e){
-
-
-    chosenColor(3)
-})
-
-function container(){
-
- let firstInnerCointainer=document.createElement('div')
- firstInnerCointainer.classList.add('div')
-    parent.appendChild(firstInnerCointainer)
-
-console.log("first container created")
-let i=0;
-while (i<=size){
-i++
-let secondInnercontainer=document.createElement('div')
-secondInnercontainer.classList.add('divIn')
-firstInnerCointainer.appendChild(secondInnercontainer)
-let secondStyle=secondInnercontainer.style;
-
-grid.addEventListener('click',function(e){
-
-
-secondStyle.setProperty("border","solid black 0.1px")
-
-})
-noGrid.addEventListener('click',function(e){
-
-    secondStyle.setProperty("border","solid black 0px")
-    
+  function setTodefaultBtns () {
+    navTitle.forEach((navTitle) => {
+      navTitle.style.setProperty('display', 'none')
     })
+    iconsSvg.forEach((iconsSvg) => {
+      iconsSvg.style.setProperty('fill', 'none')
+      iconsSvg.style.setProperty('stroke', '#9DB2CE')
+    })
+  }
 
-secondInnercontainer.addEventListener('mouseover',function(e){
-    
-        secondStyle.setProperty("background-color",color)
-})
+  function switchingDisplayOfbtns (index) {
+    setTodefaultBtns()
+    showTitle(index)
+    changingbtnColor(index)
+  }
 
-let erase=document.querySelector('.erase')
-erase.addEventListener('click',function(e){
-
-    secondStyle.setProperty("background-color","white")
-
-})
-
-
-}
-
-}
-
-for(i=0;i<size;i++){
-    container();
-
-
-    console.log("yandhi");
-    
-            }   
-        
+  function changingbtnColor (index) {
+    iconsSvg[index].style.setProperty('fill', '#386BF6')
+    iconsSvg[index].style.setProperty('stroke', '#1E1E1E')
+  }
+})()
